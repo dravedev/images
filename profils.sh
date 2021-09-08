87f3d29ab3ec6e339b71a594c4799dd202fe2ce6
Ricky Ng-Adam <rngadam@gmail.com>
2021-09-07 22:22:16 -0400

    regenerated

--- profils.sh
+++ profils.sh
@@ -1,7 +1,7 @@
 montage \
-	profils/*.jpeg profils/*.png \
+	profils/*.jpeg profils/*.jpg \
 	-geometry 200x200+5+5 \
-	-tile 5 \
+	-tile 6 \
 	collages/profils.jpeg
 
 mogrify -resize 1200 collages/profils.jpeg
